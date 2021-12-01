9a913d9295e25952ac0413a39a65bb5013138e73
Roney Santos <joseroneysilvasantos@gmail.com>
2021-12-01 17:05:45 -0300

    configuracao do k8s worker e masters finalizada

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,14 +12,14 @@ resource "aws_instance" "lab-k8s-master"{
     vpc_security_group_ids = ["${aws_security_group.cria-portas-master-k8s.id}"]
 }
 
-# resource "aws_instance" "lab-k8s-worker"{
-#     ami             = var.amis_aws["ubuntu"]
-#     count = 2
-#     instance_type   = "t2.micro"
-#     key_name        = var.key_name
-#     tags = {
-#         Name = "lab-k8s-worker-${count.index}"
-#     }
-#     vpc_security_group_ids = ["${aws_security_group.cria-portas-workers-k8s.id}"]
-#     depends_on = [aws_instance.lab-k8s-master]
-# }
+resource "aws_instance" "lab-k8s-worker"{
+    ami             = var.amis_aws["ubuntu"]
+    count = 1
+    instance_type   = "t2.micro"
+    key_name        = var.key_name
+    tags = {
+        Name = "lab-k8s-worker-${count.index}"
+    }
+    vpc_security_group_ids = ["${aws_security_group.cria-portas-workers-k8s.id}"]
+    depends_on = [aws_instance.lab-k8s-master]
+}
